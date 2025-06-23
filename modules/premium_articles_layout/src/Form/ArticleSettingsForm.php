0bb7f15398b6de7041c77613995bb3b51148e4b1
Martin Giessing <mgp@novicell.dk>
2025-06-23 15:33:47 +0200

    Added Premium Articles Layout

---
+++ modules/premium_articles_layout/src/Form/ArticleSettingsForm.php
@@ -0,0 +1,106 @@
+<?php
+namespace Drupal\premium_articles_layout\Form;
+
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ArticleSettingsForm extends ConfigFormBase {
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected DateFormatterInterface $dateFormatter;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, protected TypedConfigManagerInterface $typedConfigManager, DateFormatterInterface $dateFormatter) {
+    parent::__construct($config_factory, $typedConfigManager);
+    $this->dateFormatter = $dateFormatter;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('config.typed'),
+      $container->get('date.formatter')
+    );
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function getEditableConfigNames()
+  {
+    return ['premium_articles_layout.settings'];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getFormId()
+  {
+    return 'premium_articles_layout_settings';
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('premium_articles_layout.settings');
+    $form = [];
+
+    $form['show_article_type'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show article type'),
+      '#config_target' => 'premium_articles_layout.settings:show_article_type',
+      '#default_value' => $config->get('show_article_type') ?? TRUE,
+    ];
+
+    $form['show_article_categories'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show article categories'),
+      '#config_target' => 'premium_articles_layout.settings:show_article_categories',
+      '#default_value' => $config->get('show_article_categories') ?? TRUE,
+    ];
+
+    $form['show_list_date'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show list date'),
+      '#config_target' => 'premium_articles_layout.settings:show_list_date',
+      '#default_value' => $config->get('show_list_date') ?? TRUE,
+    ];
+
+    $time = new DrupalDateTime();
+    $format_types = \Drupal::entityTypeManager()->getStorage('date_format')->loadMultiple();
+    $options = [];
+    foreach ($format_types as $type => $type_info) {
+      $format = $this->dateFormatter->format($time->getTimestamp(), $type);
+      $options[$type] = $type_info->label() . ' (' . $format . ')';
+    }
+
+    $form['list_date_format'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Date format'),
+      '#description' => $this->t("Choose a format for displaying the date. Be sure to set a format appropriate for the field, i.e. omitting time for a field that only has a date."),
+      '#options' => $options,
+      '#config_target' => 'premium_articles_layout.settings:list_date_format',
+      '#default_value' => $config->get('list_date_format') ?: 'long',
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+}
---
+++ modules/premium_articles_layout/src/Plugin/Layout/ArticleLayout.php
@@ -0,0 +1,202 @@
+<?php
+namespace Drupal\premium_articles_layout\Plugin\Layout;
+
+use Drupal\Component\Plugin\Exception\ContextException;
+use Drupal\Core\Annotation\ContextDefinition;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityMalformedException;
+use Drupal\Core\Field\EntityReferenceFieldItemList;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Plugin\Context\ContextRepositoryInterface;
+use Drupal\Core\Plugin\Context\EntityContextDefinition;
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Routing\Router;
+use Drupal\premium_core\Plugin\Layout\BaseLayout;
+use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * A layout for articles.
+ *
+ * @Layout(
+ *   id = "layout_article_section",
+ *   label = @Translation("Article"),
+ *   category = @Translation("Content type specific"),
+ *   path = "layouts/article_section",
+ *   template = "layout--article-section",
+ *   default_region = "content",
+ *   icon_map = {
+ *     {
+ *       "content",
+ *       "sidebar"
+ *     }
+ *   },
+ *   regions = {
+ *     "content" = {
+ *       "label" = @Translation("Main content"),
+ *     },
+ *     "sidebar" = {
+ *       "label" = @Translation("Sidebar"),
+ *     }
+ *   }
+ * )
+ */
+class ArticleLayout extends BaseLayout implements ContainerFactoryPluginInterface {
+
+  /**
+   * Logger.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $logger;
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected DateFormatterInterface $dateFormatter;
+
+  /**
+   * The context repository.
+   *
+   * @var \Drupal\Core\Plugin\Context\ContextRepositoryInterface
+   */
+  protected ContextRepositoryInterface $contextRepository;
+
+  /**
+   * The article settings.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected ImmutableConfig $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactoryInterface $logger, DateFormatterInterface $dateFormatter, ContextRepositoryInterface $contextRepository, ImmutableConfig $config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->logger = $logger;
+    $this->dateFormatter = $dateFormatter;
+    $this->contextRepository = $contextRepository;
+    $this->config = $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    /** @var \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger */
+    $logger = $container->get('logger.factory');
+    /** @var \Drupal\Core\Datetime\DateFormatterInterface $dateFormatter */
+    $dateFormatter = $container->get('date.formatter');
+    /** @var \Drupal\Core\Plugin\Context\ContextRepositoryInterface $contextRepository */
+    $contextRepository = $container->get('context.repository');
+    /** @var \Drupal\Core\Config\ConfigFactoryInterface $configFactory */
+    $configFactory = $container->get('config.factory');
+    $config = $configFactory->get('premium_articles_layout.settings');
+    return new static($configuration, $plugin_id, $plugin_definition, $logger, $dateFormatter, $contextRepository, $config);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(array $regions) {
+    $build = parent::build($regions);
+
+    $build['#cache']['tags'] = $this->getCacheTags();
+
+    $build['#attributes']['class'] = [
+      'layout',
+      $this->getPluginDefinition()->getTemplate()
+    ];
+
+    $entity = $this->getNode();
+    if ($entity instanceof ContentEntityInterface) {
+      if ($entity->hasField('field_list_date') && $this->config->get('show_list_date') ?? TRUE) {
+        $time = new DrupalDateTime();
+        $build['list_date'] = [
+          '#markup' => $this->dateFormatter->format($time->getTimestamp(), $this->config->get('list_date_format') ?? 'long'),
+        ];
+      }
+      if ($entity->hasField('field_article_type') && $this->config->get('show_article_type') ?? TRUE) {
+        /** @var EntityReferenceFieldItemList $field */
+        $field = $entity->get('field_article_type');
+        /** @var Term $term */
+        foreach ($field->referencedEntities() as $term) {
+          $render = $term->toLink()->toRenderable();
+          $render['#attributes']['class'][] = 'button';
+          $build['article_type'] = $render;
+        }
+      }
+      if ($entity->hasField('field_article_categories') && $this->config->get('show_article_categories') ?? TRUE) {
+        $build['article_categories'] = [];
+        /** @var EntityReferenceFieldItemList $field */
+        $field = $entity->get('field_article_categories');
+        /** @var Term $term */
+        foreach ($field->referencedEntities() as $term) {
+          $render = $term->toLink()->toRenderable();
+          $render['#attributes']['class'][] = 'button';
+          $build['article_categories'][] = $render;
+        }
+      }
+    }
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['column_widths']['#access'] = FALSE;
+    $form['column_width']['#access'] = FALSE;
+    $form['column_spacing_top']['#access'] = FALSE;
+    $form['column_spacing_bottom']['#access'] = FALSE;
+
+    return $form;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $configuration = parent::defaultConfiguration();
+    $configuration['hide_breadcrumbs'] = FALSE;
+    $configuration['column_width'] = 'section--width-full';
+    $configuration['column_spacing_top'] = 'section--spacing-top-none';
+    $configuration['column_spacing_bottom'] = 'section--spacing-bottom-none';
+    return $configuration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    return parent::getCacheTags() + $this->config->getCacheTags();
+  }
+
+  protected function getNode(): ?ContentEntityInterface {
+    // fetch the available contexts
+    $available_contexts = $this->contextRepository->getAvailableContexts();
+
+    // ensure that the contexts have data by getting corresponding runtime contexts
+    $available_runtime_contexts = $this->contextRepository->getRuntimeContexts(array_keys($available_contexts));
+
+    $plugin_context_definition = new EntityContextDefinition('entity:node', t('Node'), FALSE);
+    $matches = $this->contextHandler()
+      ->getMatchingContexts($available_runtime_contexts, $plugin_context_definition);
+    $matching_context = reset($matches);
+    return $matching_context->getContextValue();
+  }
+}
